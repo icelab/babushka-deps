7752200a0757f8f5790f98478812f24a6f5adeaa
Andrew Croome <andrewcroome@gmail.com>
2015-06-16 16:18:34 +1000

    Remove `met?` block from buildkite agent dep.

--- buildkite_agent.rb
+++ buildkite_agent.rb
@@ -6,10 +6,6 @@ end
 dep "buildkite-agent.managed", :buildkite_token do
   requires "homebrew tap".with "buildkite/buildkite"
 
-  met? {
-    shell("brew list").include?("buildkite-agent")
-  }
-
   meet {
     shell("brew install --devel --token='#{buildkite_token}' buildkite-agent")
   }

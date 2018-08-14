ce25404d608ca08a804cc41c9352c3f3e0cc0499
Tim Riley <tim@openmonkey.com>
2018-08-14 18:07:02 +1000

    Disable installation of most things
    Trial a more stripped back base environment

--- icelab.rb
+++ icelab.rb
@@ -15,9 +15,9 @@ dep "workstation bootstrapped" do
   after {
     message = word_wrap("The environment is bootstrapped. If this is your first installation, open a new shell and run \"babushka icelab:workstation\" to continue.")
 
-    log "*"*80,        :as => :warning
-    log message,       :as => :warning
-    log "*"*79 + "!",  :as => :warning
+    log "*"*80,  :as => :warning
+    log message, :as => :warning
+    log "*"*80,  :as => :warning
   }
 
   # Thanks, ActionView
@@ -33,41 +33,39 @@ end
 dep "workstation" do
   requires "workstation bootstrapped"
 
-  requires "homebrew services"
+  # Disable installation of system services while we evaluate asdf
 
-  requires "elasticsearch.managed"
-
-  requires "memcached.managed"
-  requires "memcached.launchctl"
-
-  requires "mysql"
-
-  requires "postgresql.managed"
-  requires "postgresql.launchctl"
-
-  requires "redis.managed"
-  requires "redis.launchctl"
+  # requires "homebrew services"
+  # requires "elasticsearch.managed"
+  # requires "memcached.managed"
+  # requires "memcached.launchctl"
+  # requires "mysql"
+  # requires "postgresql.managed"
+  # requires "postgresql.launchctl"
+  # requires "redis.managed"
+  # requires "redis.launchctl"
 
   # General homebrew packages
   requires "docker.cask"
   requires "imagemagick.managed"
   requires "watch.managed"
-  requires "heroku.managed"
+  # Disable heroku until
+  # requires "heroku.managed"
   requires "phantomjs.managed"
   requires "yarn"
 
   # Ruby
-  requires "ruby 2.4.0"
-  requires "global ruby version".with "2.4.0"
+  # requires "ruby 2.4.0"
+  # requires "global ruby version".with "2.4.0"
 
   # Global gems
-  requires "git-smart.gem"
-  requires "raygun.gem"
+  # requires "git-smart.gem"
+  # requires "raygun.gem"
 
   # Node
-  requires "node 6.10.0"
-  requires "global node version".with "6.10.0"
+  # requires "node 6.10.0"
+  # requires "global node version".with "6.10.0"
 
   # Global node packages
-  requires "coffee-script.npm"
+  # requires "coffee-script.npm"
 end
